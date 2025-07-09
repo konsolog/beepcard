5d49a0ca059a9757f9c9bdf304d483e96be7c372
rekkord <geraldjamestan@gmail.com>
2025-07-09 19:17:19 +0800

    exit

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print("test")
+#this will be fun
+
+fare
+
+def main():
+    print(f"Welcome to the LRT-1 System!\n\n****************************\nLoad Balance: PHP {fare}\nChoices\n1-Load Beep Card\n2-Display All Stations\n3-Trip and Fare\n4-Transaction History\n5-Exit")
+    call = input("Choose from the options: ")
+
+    return call # This returns (outputs) the selected choice
+
+if main() == 5:
+    exit()
