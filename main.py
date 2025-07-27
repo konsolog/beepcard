5ca328b525b5453a1d89b2852938cf203d0b79dd
rekkord <geraldjamestan@gmail.com>
2025-07-27 18:57:02 +0800

    final

--- main.py
+++ main.py
@@ -4,12 +4,12 @@
 # myself. The program was run, tested, and debugged by my efforts. I
 # further certify that I have not copied in part or whole or otherwise
 # plagiarized the work of other students and/or persons.
+# Josh Caleb B. Aggabao, DLSU ID# 12462098
 # Gerald James Z. Tan, DLSU ID# 12470732
 # David Angelo D. Torreon, DLSU ID# 12466514 
-# Josh Caleb B. Aggabao, DLSU ID# 12462098
 
 # Description: A loadable train card system
-# Programmed by: Gerald James Z. Tan, STEM11-P
+# Programmed by: Gerald James Z. Tan, David Angelo D. Torreon, Josh Caleb B. Aggabao, STEM11-P
 # Last modified: July 18, 2025
 # Acknowledgements: https://docs.python.org/
 load = 0.0
@@ -64,7 +64,7 @@ def trim(s, n):
             s[size] 
             size += 1
         except IndexError:
-            end = True # SIX SEVENNN
+            end = True 
 
     # go backwards to find the (n+1)th newline
     c = 0
@@ -94,13 +94,13 @@ def trim(s, n):
         except IndexError:
             finished = True
 
-    return res # God save me for I am not your strongest soldier
+    return res 
 
 on = True 
 while on: # infinite looping until 5 is input/main loop
     sel = main()
 
-    if sel == 1: # top up (why is this so complicated)
+    if sel == 1: # top up 
         done = False
         while not done:
             a = input("Enter the amount (greater than 0) to load on your beep card: ")
@@ -112,10 +112,10 @@ while on: # infinite looping until 5 is input/main loop
                     while q not in ("Y", "N"):
                         q = input("Complete the transaction (Y/N)? ").upper() # covers all cases: y or Y, n or N
                         if q == "Y":
-                            load += v
+                            load += v # add up 
                             print(f"PHP {v:.2f} successfully loaded to your beep card.")
-                            history += f"Top-up     {v:.2f}     {load:.2f}\n"
-                            history = trim(history, 5)
+                            history += f"Top-up     +{v:.2f}     {load:.2f}\n"# append to history
+                            history = trim(history, 5) # cutoff 5
                             done = True  # exit all loops
                         elif q == "N":
                             print("Cancelled Transaction")
@@ -129,17 +129,17 @@ while on: # infinite looping until 5 is input/main loop
 
     elif sel == 2: # print the stations
         print("""****************************
-    All LRT-1 Stations
-    1-Baclaran
-    2-EDSA
-    3-Libertad
-    4-Gil Puyat
-    5-Vito Cruz 
-    6-Quirino 
-    7-Pedro Gil 
-    8-UN Avenue 
-    9-Central Terminal 
-    10-Carriedo""")
+All LRT-1 Stations
+1-Baclaran
+2-EDSA
+3-Libertad
+4-Gil Puyat
+5-Vito Cruz 
+6-Quirino 
+7-Pedro Gil 
+8-UN Avenue 
+9-Central Terminal 
+10-Carriedo""")
         
     elif sel == 3: # select the station and calculate the fare
         print(f"""
@@ -154,19 +154,58 @@ Note: The origin and destination should not be the same.
         origin = int(input("Enter the station's number corresponding to your origin:"))
         destination = int(input("Enter the station's number corresponding to your destination:"))
 
-        originStation = stations[origin-1] 
-        destinationStation = stations[destination-1]
+        originStation = stations[origin-1] # this makes the first element become 1 (baclaran) and not 0
+        destinationStation = stations[destination-1] # ditto
 
-        count = abs(origin - destination)
+        count = abs(origin - destination) # get the absolute value of how many steps in between stations
+        fare = 15.00
+        finalfare = 15.00
+
+        if count <= 3:
+            finalfare = 15.00
+        else:
+            extrastations = count - 3
+            extrafare = (extrastations // 3 + 1) * 5.00
+            finalfare += extrafare
+
+        vatsales = finalfare / 1.12
+        vatamount = finalfare - vatsales
 
         print(f"""
 ------------------------------------------- 
 Fare calculation
 """)
         print(f"Journey Details: {originStation} -> {destinationStation}")
+        print(f"Minimum fare: PHP 15.00")
+        print(f"Additional fare: PHP {extrafare:.2f}")
+        print(f"Total Fare: PHP {finalfare:.2f}")
+        q = ""
+        while q not in ("Y", "N"):
+            q = input("Complete the transaction (Y/N)? ").upper() # covers all cases: y or Y, n or N
+            if q == "Y":
+                load -= finalfare # deduct amnt of trip
+                print(f"PHP {v:.2f} was deducted for your trip from {originStation} to {destinationStation}")
+                history += f"Trip     -{finalfare:.2f}     {load:.2f}\n" # append to history
+                history = trim(history, 5) # cutoff when it exeeds 5 lines
+
+                print(f"""
+------------------------------------------- 
+Receipt
+-------------------------------------------
+Journey Details: {originStation} -> {destinationStation}
+Amount due: PHP {finalfare:.2f}
+Vatable sales: PHP {vatsales:.2f}
+VAT amount: PHP {vatamount:.2f}
+------------------------------------------- 
+""")
+                
+            elif q == "N":
+                print("Cancelled Transaction")
+            else:
+                print("Invalid input (must be Y/N)")
 
             
-    elif sel == 4: # most code is handled at the top outside the loop
+    elif sel == 4: # most code is handled at 3 and 1
         print("""**************************** 
 Last 5 Transactions
 Transaction     Amount(PHP)     Ending Balance(PHP)""")
@@ -177,4 +216,4 @@ Transaction     Amount(PHP)     Ending Balance(PHP)""")
         on = False 
 
     else: # invalid choice made
-        print("⚠️ ACHTUNG! Not in the given choices.")
+        print("Not in the given choices.")
